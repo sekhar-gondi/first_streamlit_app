ad375f3f2f0ead4667f6aaf0fa2681f68773ccef
sekhar-gondi <136183708+sekhar-gondi@users.noreply.github.com>
2023-06-11 21:44:52 +0530

    Update streamlit_app.py
    making entries into SF

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ if streamlit.button('Get Fruit load list'):
 
 def insert_row_into_SF(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    my_cur.execute("insert into fruit_load_list values('"+ new_fruit +"')")
     return 'Thanks for adding new fruit:' + new_fruit
 
 add_my_fruit = streamlit.text_input('What Fruit would you like to add?')  
