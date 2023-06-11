dcfc6ce307d9e91f27217f8afe285dad7b2c3456
sekhar-gondi <136183708+sekhar-gondi@users.noreply.github.com>
2023-06-11 21:06:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?') #asking user to input fruit name
   if not fruit_choice:
     streamlit.error("Please select fruit to get information.")
-  else
+  else:
     #streamlit.write('The user entered', fruit_choice) #displaying what user asked for
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
     #streamlit.text(fruityvice_response.json()) #just writes data to screen but not in proper format
